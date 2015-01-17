cfb66f3e4d0f1768b378b88a7b90010b0ea429bc
rbhuta <rbhuta95@gmail.com>
2015-01-17 17:48:23 -0500

    php testing

---
+++ welcome_get.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+Book: <?php echo $_POST["alpha"]; ?><br>
+Course Name: <?php echo $_GET["beta"]; ?><br>
+Professor: <?php echo $_GET["charlie"]; ?><br>
+ISBN: <?php echo $_GET["isbn"]; ?><br>
+Usefulness: <?php echo $_GET["response-rate1"]; ?><br>
+Bang: <?php echo $_GET["response-rate2"]; ?><br>
+Clarity: <?php echo $_GET["response-rate3"]; ?><br>
+Needit: <?php echo $_GET["switch1"]; ?><br>
+Comments: <?php echo $_GET["textarea"]; ?><br>
+
+</body>
+</html>
