28e6506cf01e34556aec846adf2edc12ebe2e7b2
rbhuta <rbhuta95@gmail.com>
2015-01-18 05:14:47 -0500

    vx

--- welcome_get.php
+++ welcome_get.php
@@ -1,15 +1,40 @@
-<html>
-<body>
+<?php
+$textbook = mysql_real_escape_string($_POST['textbookname']);
+$username = mysql_real_escape_string($_POST['coursename']);
+$password = mysql_real_escape_string($_POST['professor']);
+$isbn = mysql_real_escape_string($_POST['isbn']);
+$usefulness = mysql_real_escape_string($_POST['response-rate1']);
+$value = mysql_real_escape_string($_POST['response-rate2']);
+$clarity = mysql_real_escape_string($_POST['response-rate3']);
+$need = mysql_real_escape_string($_POST['switch1']);
+$comment = mysql_real_escape_string($_POST['textarea']);
 
-Book: <?php echo $_POST["alpha"]; ?><br>
-Course Name: <?php echo $_GET["beta"]; ?><br>
-Professor: <?php echo $_GET["charlie"]; ?><br>
-ISBN: <?php echo $_GET["isbn"]; ?><br>
-Usefulness: <?php echo $_GET["response-rate1"]; ?><br>
-Bang: <?php echo $_GET["response-rate2"]; ?><br>
-Clarity: <?php echo $_GET["response-rate3"]; ?><br>
-Needit: <?php echo $_GET["switch1"]; ?><br>
-Comments: <?php echo $_GET["textarea"]; ?><br>
 
-</body>
-</html>
+// Create connection
+$conn = new mysqli("localhost", "root", "zHfApkXelg");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+$sql = "SELECT ISBN FROM book";
+$result = $conn->query($sql);
+
+if ($result == $isbn) {
+	$sql = "UPDATE review SET courseName='coursename', ProfessorName= 'professor', UsefulnessRating= 'response-rate1', ValueRating= 'response-rate2', ClarityRating='response-rate3', Need='switch1', Comment='textarea' WHERE ($result==$isbn);" }
+	else {
+	$sql = "INSERT INTO book (TextbookName, ISBN, CourseName, ProfessorName)
+	VALUES ('textbookname', 'isbn', 'coursename', 'professor')";
+	$sql = "INSERT INTO review (ISBN, UsefulnessRating, ValueRating, ClarityRating, Need, Comment)
+	VALUES ('textbookname', 'isbn', 'response-rate1', 'response-rate2', 'response-rate3', 'switch1', 'textarea')";
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
